7c3a27b59284ab686f555ba2f2b56803e425223d
wyne-ybanez <wyne_01@icloud.com>
2021-09-29 18:04:00 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import ReactDom from 'react-dom'
+
+const Booklist = () => {
+  return (
+    <section>
+    </section>
+  )
+} 
+
+ReactDom.render(
+  <Booklist />, document.getElementById('root')
+)
