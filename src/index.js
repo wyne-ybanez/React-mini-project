6761e1e8e725e798843be3fdb007bc3fd8b48d91
wyne-ybanez <wyne_01@icloud.com>
2021-10-04 13:37:22 +0100

    Add separate files for booklist data, refactor styling, add code to render individual books

--- src/index.js
+++ src/index.js
@@ -1,39 +1,23 @@
-import ReactDom from 'react-dom'
-import './index.css'
+import React from 'react';
+import ReactDom from 'react-dom';
 
-const Booklist = () => {
+// CSS
+import './index.css';
+
+import {data} from './books'
+import SpecificBook from './Book'
+import {greeting} from './testing/testing'
+
+function BookList() {
+  console.log(greeting);
   return (
-    <section className="booklist">
-        <Book />
-        <Book />
-        <Book />
-        <Book />
-        <Book />
-        <Book />
-        <Book />
-        <Book />
-        <Book />
-        <Book />
+    <section className='booklist'>
+      {data.map((book, index) => {
+        return <SpecificBook key={book.id} {...book}></SpecificBook>;
+      })}
     </section>
-  )
-} 
-
-const Book = () => {
-  return (
-    <article className="book">
-      <Image></Image>
-      <Title></Title>
-      <Author></Author>
-    </article>
-  )
+  );
 }
 
-const Image = () => <img src="https://images-na.ssl-images-amazon.com/images/I/81iAADNy2NL._AC_UL200_SR200,200_.jpg" 
-      alt="Book" />
 
-const Title = () => <h1>Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones')</h1>
-const Author = () => <h4 style={{color: '#617D98', fontSize: '0.75rem', marginTop: '0.25rem'}}>James Clear</h4>
-
-ReactDom.render(
-  <Booklist />, document.getElementById('root')
-)
+ReactDom.render(<BookList />, document.getElementById('root'));
